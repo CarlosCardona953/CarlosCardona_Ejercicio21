ef52eb41456abdd4bcaf81cf74674e9dbfcede75
CarlosDCN <48950589+CarlosCardona953@users.noreply.github.com>
2019-04-02 07:40:03 -0500

    Ejercicio 21

---
+++ CarlosCardona_Ejercicio21.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+os.system("g++ caminata.cpp -o caminata.x")
+os.system("./caminata.x > datos.dat")
+
+data = np.loadtxt("datos.dat")
+
+plt.figure()
+plt.plot(data[:,0], data[:,1])
+plt.title("Caminata Aleatoria")
+plt.savefig("Caminata Aleatoria.png")
